2bad053b4dac3772c682ad9532ed7a5159240177
Rounik <rounik1020@gmail.com>
2022-06-19 17:37:01 +0530

    Created associations for Cart

--- app.js
+++ app.js
@@ -15,6 +15,8 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const Product = require('./models/product');
 const User = require('./models/user');
+const Cart = require('./models/cart');
+const CartItem = require('./models/cart-item');
 
 app.use((req, res, next) => {
   User.findByPk(1)
@@ -33,9 +35,17 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-User.hasMany(Product, { constraints: true, onDelete: 'CASCADE' });
+Product.belongsTo(User, { constrains: true, onDelete: 'CASCADE' });
+User.hasMany(Product);
+
+User.hasOne(Cart);
+Cart.belongsTo(User);
+
+Cart.belongsToMany(Product, { through: CartItem });
+Product.belongsToMany(Cart, { through: CartItem });
+
 sequelize
-  .sync()
+  .sync((force = true))
   .then(() => {
     return User.findByPk(1);
   })
