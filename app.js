d1c90d2afe7f05ec53904058096a15e34e81782e
Rounik <rounik1020@gmail.com>
2022-06-19 16:43:37 +0530

    Createing dummy user

--- app.js
+++ app.js
@@ -13,6 +13,17 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const Product = require('./models/product');
+const User = require('./models/user');
+
+app.use((req, res, next) => {
+  User.findByPk(1)
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.log(err));
+});
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,8 +33,19 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-sequelize.sync().then(() => {
-  app.listen(3000, () => {
-    console.log('Server running on http://localhost:3000');
+User.hasMany(Product, { constraints: true, onDelete: 'CASCADE' });
+sequelize
+  .sync()
+  .then(() => {
+    return User.findByPk(1);
+  })
+  .then((user) => {
+    if (!user) {
+      return User.create({ name: 'Rounik', email: 'rounik1020@gmail.com' });
+    }
+  })
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server running on http://localhost:3000');
+    });
   });
-});
