ee1fbf3d5066af87151cb89d37473132d1a112e6
Rounik <rounik1020@gmail.com>
2022-07-05 01:03:21 +0530

    Created User model

--- models/user.js
+++ models/user.js
@@ -1,21 +1,22 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../util/database');
+const { ObjectId } = require('mongodb');
 
-const User = sequelize.define('user', {
-  id: {
-    type: Sequelize.INTEGER,
-    primaryKey: true,
-    autoIncrement: true,
-    allowNull: false,
-  },
-  name: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-  email: {
-    type: Sequelize.STRING,
-    allowNull: false,
-  },
-});
+const getDb = require('../util/database').getDb;
+
+class User {
+  constructor(name, email) {
+    this.name = name;
+    this.email = email;
+  }
+
+  save() {
+    const db = getDb();
+    return db.collection('users').insertOne(this);
+  }
+
+  static findById(id) {
+    const db = getDb();
+    return db.collection('users').findOne({ _id: ObjectId(id) });
+  }
+}
 
 module.exports = User;
