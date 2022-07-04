692e32c13012e17d3688d3176a9e149ac8848945
Rounik <rounik1020@gmail.com>
2022-07-05 01:03:33 +0530

    Binding with one user

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
 const mongoConnect = require('./util/database').mongoConnect;
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use((req, res, next) => {
+  User.findById('62c33df0c07098eda1cae495')
+    .then((user) => {
+      req.user = user;
+      next();
+    })
+    .catch((err) => console.error(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
