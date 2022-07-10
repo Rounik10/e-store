3f2cbb23381e4d2c7a31fbefc9d323ab91d81358
Rounik <rounik1020@gmail.com>
2022-07-11 02:13:18 +0530

    Mongoose setup

--- app.js
+++ app.js
@@ -4,10 +4,10 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const errorController = require('./controllers/error');
-const mongoConnect = require('./util/database').mongoConnect;
-const User = require('./models/user');
+// const User = require('./models/user');
 
 const app = express();
 
@@ -20,22 +20,25 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use((req, res, next) => {
-  User.findById('62c33df0c07098eda1cae495')
-    .then((user) => {
-      req.user = new User(user.name, user.email, user.cart, user._id);
-      next();
-    })
-    .catch((err) => console.error(err));
-});
+// app.use((req, res, next) => {
+//   User.findById('62c33df0c07098eda1cae495')
+//     .then((user) => {
+//       req.user = new User(user.name, user.email, user.cart, user._id);
+//       next();
+//     })
+//     .catch((err) => console.error(err));
+// });
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-mongoConnect(() => {
-  app.listen(3000, () => {
-    console.log('Server started on http://localhost:3000');
-  });
-});
+mongoose
+  .connect(process.env.MONGO_DB_URL)
+  .then(() => {
+    app.listen(3000, () => {
+      console.log('Server started on http://localhost:3000');
+    });
+  })
+  .catch(console.error);
